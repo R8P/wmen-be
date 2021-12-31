9453b0f7817410c3599eb88dbf2748d18fefca00
abdulkadir <abdulkadirozkan@yandex.com>
2021-12-31 17:57:59 +0300

    install typescript-express-starter

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import 'dotenv/config';
+import '@/index';
+import App from '@/app';
+import {IndexController} from '@controllers/index.controller';
+import validateEnv from '@utils/validateEnv';
+
+validateEnv();
+
+const app = new App([IndexController]);
+app.listen();
