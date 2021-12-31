0d07cae29f0f4fc7ebd50f4d9df7f3dfe75993b5
abdulkadir <abdulkadirozkan@yandex.com>
2021-12-31 10:51:22 +0300

    add partners.json

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express, {Application, Request, Response, NextFunction} from 'express';
-
+import * as fs from 'fs';
+import * as path from 'path';
+import IPartner from "./interface/partner.interface";
 
 const app: Application = express();
 
@@ -7,4 +9,12 @@ app.get('/', (req: Request, res: Response, next: NextFunction) => {
     res.send('hello');
 });
 
+function getAllPartners(): Array<IPartner> {
+    return JSON.parse(fs.readFileSync(path.join(__dirname, 'data/partners.json'), {
+        encoding: 'utf8',
+        flag: 'r'
+    }));
+}
+
+console.log(getAllPartners())
 app.listen(5000, () => console.log('Server running'));
